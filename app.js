18603276dcc06df160c58cd51b6a92030aff0240
bilguun-erdem0625 <143863121+bilguun-erdem0625@users.noreply.github.com>
2023-10-14 10:53:12 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+var neg, hoyr;
+neg = 3;
+hoyr = 9;
+hariu = neg * hoyr;
+console.log(hariu);
+
+
+
+var nob, bon, XD;
+nob = 3;
+bon = 2;
+XD = 3;
+HAHA = neg * hoyr * XD;
+console.log(HAHA);
+  
+  var number = +prompt("second oruuln uu");
+  // alert("tailbar"); --> sanamj
+  var sec,min;
+  min = number / 60;
+  //200 / 60 = 3.33333 --> ?
+  sec = number % 60;
+  alert(Math.floor(min)+"min"+sec+"sec");
+  // 200second --> 3 min20 sec// 1min - 60 second
+  
+  var number = +prompt("second oruuln uu");
+  // alert("tailbar"); --> sanamj
+  var sec,min,tsag;
+  min = number / 60;
+  //3612 / 60 = 60.2 --> ?
+  sec = number % 60;
+  tsag = number / 60;
+  //3612 / 60 = 60.2 --> ?
+  alert(Math.floor(min)+"min"+sec+"sec");
+  // 3612second --> 60min12 sec// 1min - 60 second// 1tsag 
